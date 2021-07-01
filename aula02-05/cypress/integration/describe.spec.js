ead59cdb17eadb1711b0bffeca14788e61ecc62a
DouglasFlorido <crascaroth@gmail.com>
2021-07-01 13:12:34 -0300

    Describe It

---
+++ aula02-05/cypress/integration/describe.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+it("A external test...", () => {
+
+})
+
+describe('Should Groupe Test', () => {
+    describe('should group more specific tests...', () => {
+        it('A specific test case...', () => {
+
+
+        })
+    })
+    describe('should group more specific tests...2', () => {
+        it('A specific test case...2', () => {
+
+
+        })
+    })
+    it('A internal Test', () => {
+        
+    })
+})
