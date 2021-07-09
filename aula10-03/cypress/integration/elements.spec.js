30572fd8b4db6552458d331c61702f90ee6e93e2
DouglasFlorido <crascaroth@gmail.com>
2021-07-09 16:50:53 -0300

    aula 11-03 finalizada

--- aula10-03/cypress/integration/elements.spec.js
+++ aula10-03/cypress/integration/elements.spec.js
@@ -8,4 +8,15 @@ describe('Work With Basic  Elements', () => {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
+
+    it('Links', () => {
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', "Voltou!")
+        
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', "Voltou!")
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', "Voltou!")
+    })
 })
