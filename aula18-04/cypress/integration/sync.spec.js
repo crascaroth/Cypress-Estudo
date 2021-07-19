4b5ef89698fd31a9af70170bd1c3bc67c8ee6297
DouglasFlorido <crascaroth@gmail.com>
2021-07-19 10:46:42 -0300

    Aula 18 Terminada

---
+++ aula18-04/cypress/integration/sync.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Waits ...', () => {
+
+    before(()=>{
+        cy.visit('http://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(()=>{
+        cy.reload()
+    })
+
+    it('Element must be available', () => {
+        cy.get('#novoCampo').should('not.exist');
+        cy.get('#buttonDelay').click();        
+        cy.get('#novoCampo').should('not.exist');
+        cy.get('#novoCampo').should('exist');
+        cy.get('#novoCampo').type('funcional');
+    })
+
+
+})
